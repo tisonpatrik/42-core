87c425b7ee22da8d1dd226eedc8a8122a36b8d03
Patrik Tison <ptison@c2r5s4.42prague.com>
2025-08-16 16:19:49 +0200

    normi

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptison <ptison@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/14 20:16:34 by ptison            #+#    #+#             */
-/*   Updated: 2025/08/16 13:26:36 by ptison           ###   ########.fr       */
+/*   Created: 2025/08/16 16:13:20 by ptison            #+#    #+#             */
+/*   Updated: 2025/08/16 16:13:40 by ptison           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ t_list	*new_int_node(int number)
 	int		*value;
 	t_list	*node;
 
-	value = malloc(sizeof *value);
+	value = malloc(sizeof * value);
 	if (!value)
 		return (NULL);
 	*value = number;
@@ -72,6 +72,25 @@ int	is_duplicate(t_list *list, int value)
 	return (0);
 }
 
+t_list	*get_node(t_list *list, t_list *node, char *str)
+{
+	int	number;
+
+	number = ft_atoi(str);
+	if (!is_valid_input(str, number))
+	{
+		ft_lstclear(&list, free);
+		return (NULL);
+	}
+	if (is_duplicate(list, number))
+	{
+		ft_lstclear(&list, free);
+		return (NULL);
+	}
+	node = new_int_node(number);
+	return (node);
+}
+
 t_list	*get_input_values(char **ag)
 {
 	t_list	*list;
@@ -83,18 +102,7 @@ t_list	*get_input_values(char **ag)
 	i = 1;
 	while (ag[i])
 	{
-		number = ft_atoi(ag[i]);
-		if (!is_valid_input(ag[i], number))
-		{
-			ft_lstclear(&list, free);
-			return (NULL);
-		}
-		if (is_duplicate(list, number))
-		{
-			ft_lstclear(&list, free);
-			return (NULL);
-		}
-		node = new_int_node(number);
+		node = get_node(list, node, ag[i]);
 		if (!node)
 		{
 			ft_lstclear(&list, free);
@@ -106,10 +114,11 @@ t_list	*get_input_values(char **ag)
 	return (list);
 }
 
-t_swap	*get_init_swap()
+t_swap	*get_init_swap(void)
 {
 	t_swap	*input;
-	input = malloc(sizeof *input);
+
+	input = malloc(sizeof * input);
 	if (!input)
 		return (NULL);
 	input->stack_a = NULL;
