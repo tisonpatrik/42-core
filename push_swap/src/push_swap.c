ac9bb5f901ffaa7a9ff49c2251b962266bd6825b
Patrik Tison <ptison@c2r5s4.42prague.com>
2025-08-16 13:59:50 +0200

    fully workable input validation

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ptison <ptison@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 20:16:34 by ptison            #+#    #+#             */
-/*   Updated: 2025/08/15 16:13:40 by patrik           ###   ########.fr       */
+/*   Updated: 2025/08/16 13:26:36 by ptison           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	is_valid_input(char *original, int number)
 	int		len;
 	char	*reverse_int;
 	int		reverse_len;
+	int		ok;
 
 	len = ft_strlen(original);
 	if ((number == 0) && (ft_strncmp(original, "0", len) != 0))
@@ -35,58 +36,79 @@ int	is_valid_input(char *original, int number)
 	}
 	reverse_int = ft_itoa(number);
 	reverse_len = ft_strlen(reverse_int);
-	if ((reverse_len != len) || (ft_strncmp(reverse_int, original, len) != 0))
-	{
+	ok = (reverse_len == len) && (ft_strncmp(reverse_int, original, len) == 0);
+	free(reverse_int);
+	if (!ok)
 		return (0);
-	}
 	return (1);
 }
+
+t_list	*new_int_node(int number)
+{
+	int		*value;
+	t_list	*node;
+
+	value = malloc(sizeof *value);
+	if (!value)
+		return (NULL);
+	*value = number;
+	node = ft_lstnew(value);
+	if (!node)
+	{
+		free(value);
+		return (NULL);
+	}
+	return (node);
+}
+
+int	is_duplicate(t_list *list, int value)
+{
+	while (list)
+	{
+		if (*(int *)list->content == value)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
+
 t_list	*get_input_values(char **ag)
 {
-	t_list	*result;
-	t_list	*node;
+	t_list	*list;
 	int		i;
 	int		number;
-	int		is_valid;
-	int		*value;
+	t_list	*node;
 
-	result = NULL;
+	list = NULL;
 	i = 1;
 	while (ag[i])
 	{
 		number = ft_atoi(ag[i]);
-		is_valid = is_valid_input(ag[i], number);
-		if (is_valid == 0)
+		if (!is_valid_input(ag[i], number))
 		{
-			ft_lstclear(&result, free);
+			ft_lstclear(&list, free);
 			return (NULL);
 		}
-		value = malloc(sizeof *value);
-		if (!value)
+		if (is_duplicate(list, number))
 		{
-			ft_lstclear(&result, free);
+			ft_lstclear(&list, free);
 			return (NULL);
 		}
-		*value = number;
-		node = ft_lstnew(value);
+		node = new_int_node(number);
 		if (!node)
 		{
-			free(value);
-			ft_lstclear(&result, free);
+			ft_lstclear(&list, free);
 			return (NULL);
 		}
-		ft_lstadd_back(&result, node);
+		ft_lstadd_back(&list, node);
 		i++;
 	}
-	return (result);
+	return (list);
 }
 
-t_swap	*get_init_swap(int ac)
+t_swap	*get_init_swap()
 {
 	t_swap	*input;
-
-	if (ac < 2)
-		return (NULL);
 	input = malloc(sizeof *input);
 	if (!input)
 		return (NULL);
@@ -96,11 +118,14 @@ t_swap	*get_init_swap(int ac)
 	input->bsize = 0;
 	return (input);
 }
+
 int	main(int ac, char **args)
 {
 	t_swap	*input;
 
-	input = get_init_swap(ac);
+	if (ac < 2)
+		return (0);
+	input = get_init_swap();
 	if (!input)
 		return (exit_with_error());
 	input->stack_a = get_input_values(args);
