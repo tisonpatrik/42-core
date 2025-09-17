f39d58a4225417c37cbb6cfa03da733096473448
tisonpatrik <patriktison@gmail.com>
2025-09-17 20:11:37 +0200

    normi

--- push_swap/src/selector/candidates_builder.c
+++ push_swap/src/selector/candidates_builder.c
@@ -6,7 +6,7 @@
 /*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 22:15:11 by patrik            #+#    #+#             */
-/*   Updated: 2025/09/16 22:20:27 by patrik           ###   ########.fr       */
+/*   Updated: 2025/09/17 20:10:53 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,32 @@
 #include <stdbool.h>
 
 
-int	compare_candidates(const void *a, const void *b)
+static int	compare_primary_criteria(const t_candidate *a, const t_candidate *b)
 {
-	const t_candidate	*atemp = (const t_candidate *)a;
-	const t_candidate	*btemp = (const t_candidate *)b;
-	int					absa;
-	int					absb;
+	if (a->score != b->score)
+	{
+		if (a->score < b->score)
+			return (-1);
+		else
+			return (1);
+	}
+	if (a->position.total != b->position.total)
+	{
+		if (a->position.total < b->position.total)
+			return (-1);
+		else
+			return (1);
+	}
+	return (0);
+}
 
-	if (atemp->score != btemp->score)
-	{
-		if (atemp->score < btemp->score)
-			return (-1);
-		else
-			return (1);
-	}
-	if (atemp->position.total != btemp->position.total)
-	{
-		if (atemp->position.total < btemp->position.total)
-			return (-1);
-		else
-			return (1);
-	}
-	absa = ft_abs(atemp->position.cost_a);
-	absb = ft_abs(btemp->position.cost_a);
+static int	compare_cost_criteria(const t_candidate *a, const t_candidate *b)
+{
+	int	absa;
+	int	absb;
+
+	absa = ft_abs(a->position.cost_a);
+	absb = ft_abs(b->position.cost_a);
 	if (absa != absb)
 	{
 		if (absa < absb)
@@ -45,21 +48,42 @@ int	compare_candidates(const void *a, const void *b)
 		else
 			return (1);
 	}
-	if (atemp->position.to_index != btemp->position.to_index)
+	return (0);
+}
+
+static int	compare_index_criteria(const t_candidate *a, const t_candidate *b)
+{
+	if (a->position.to_index != b->position.to_index)
 	{
-		if (atemp->position.to_index < btemp->position.to_index)
+		if (a->position.to_index < b->position.to_index)
 			return (-1);
 		else
 			return (1);
 	}
-	if (atemp->position.from_index != btemp->position.from_index)
+	if (a->position.from_index != b->position.from_index)
 	{
-		if (atemp->position.from_index < btemp->position.from_index)
+		if (a->position.from_index < b->position.from_index)
 			return (-1);
 		else
 			return (1);
 	}
 	return (0);
+}
+
+int	compare_candidates(const void *a, const void *b)
+{
+	const t_candidate	*atemp = (const t_candidate *)a;
+	const t_candidate	*btemp = (const t_candidate *)b;
+	int					result;
+
+	result = compare_primary_criteria(atemp, btemp);
+	if (result != 0)
+		return (result);
+	result = compare_cost_criteria(atemp, btemp);
+	if (result != 0)
+		return (result);
+	result = compare_index_criteria(atemp, btemp);
+	return (result);
 }
 
 void	sort_candidates_by_cost(t_candidate *candidates, int count)
--- push_swap/src/selector/lookahead_evaluator.c
+++ push_swap/src/selector/lookahead_evaluator.c
@@ -6,182 +6,85 @@
 /*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 20:49:22 by ptison            #+#    #+#             */
-/*   Updated: 2025/09/17 18:59:01 by patrik           ###   ########.fr       */
+/*   Updated: 2025/09/17 19:57:01 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/selector.h"
 #include <stdbool.h>
-#include <string.h>
 
-static void	remove_element_at_index(int *array, int *size, int index)
+
+static int	calculate_heuristic_without_copy(int *a_values, int size_a,
+		int size_penalty_factor, int heuristic_offset, int heuristic_divisor)
 {
-	int	i;
-
-	if (index < 0 || index >= *size)
-		return ;
-	i = index;
-	while (i < *size - 1)
-	{
-		array[i] = array[i + 1];
-		i++;
-	}
-	(*size)--;
-}
-
-static void	insert_element_at_index(int *array, int *size, int index, int value)
-{
-	int	i;
-
-	if (index < 0 || index > *size)
-		return ;
-	if (*size >= 1000)
-		return ;
-	i = *size;
-	while (i > index)
-	{
-		array[i] = array[i - 1];
-		i--;
-	}
-	array[index] = value;
-	(*size)++;
-}
-
-int	get_break_points(int *a, int size)
-{
-	int	n;
 	int	breakpoints;
+	int	i;
 
-	if (size <= 1)
-	{
+	if (size_a <= 1)
 		return (0);
-	}
-	n = size;
 	breakpoints = 0;
-	for (int i = 0; i < n - 1; i++)
+	for (i = 0; i < size_a - 1; i++)
 	{
-		if (a[i] > a[i + 1])
-		{
+		if (a_values[i] > a_values[i + 1])
 			breakpoints++;
-		}
 	}
-	if (a[n - 1] > a[0])
-	{
+	if (a_values[size_a - 1] > a_values[0])
 		breakpoints++;
-	}
-	return (breakpoints);
+	return ((breakpoints + size_a / size_penalty_factor + heuristic_offset)
+		/ heuristic_divisor);
 }
 
-int	get_estimatation(int *temp_a, int size, int size_penalty_factor,
-		int heuristic_offset, int heuristic_divisor)
+static int	calculate_score_without_simulation(t_position position,
+		int *a_values, int size_a, t_selector_arena *arena)
 {
-	int	breakpoints;
-	int	len;
-	int	size_penalty;
-	int	estimation;
-
-	breakpoints = get_break_points(temp_a, size);
-	len = size;
-	size_penalty = len / size_penalty_factor;
-	estimation = (breakpoints + size_penalty + heuristic_offset)
-		/ heuristic_divisor;
-	return (estimation);
-}
-
-static t_position	validate_lookahead_input(t_candidate *candidates,
-		t_selector_arena *arena)
-{
-	t_position	invalid_position;
-
-	invalid_position.total = INT_MAX;
-	if (!arena || !arena->snapshot_arena || !candidates)
-		return (invalid_position);
-	if (arena->top_k_count == 0)
-		return (invalid_position);
-	return (candidates[0].position);
-}
-
-static void	copy_snapshot_to_temp(t_selector_arena *arena,
-		int **temp_a_values, int **temp_b_values,
-		int *temp_size_a, int *temp_size_b)
-{
-	*temp_size_a = arena->snapshot_arena->size_a;
-	*temp_size_b = arena->snapshot_arena->size_b;
-	*temp_a_values = arena->temp_a_values;
-	*temp_b_values = arena->temp_b_values;
-	memcpy(*temp_a_values, arena->snapshot_arena->a_values,
-		*temp_size_a * sizeof(int));
-	memcpy(*temp_b_values, arena->snapshot_arena->b_values,
-		*temp_size_b * sizeof(int));
-}
-
-static int	simulate_move_operation(t_position position, int *temp_a_values,
-		int *temp_b_values, int *temp_size_a, int *temp_size_b)
-{
-	int	rot;
-	int	ib;
-	int	ia;
-	int	x;
-
-	rot = merged_cost(position.cost_a, position.cost_b);
-	ib = normalize_index(*temp_size_b, position.cost_b);
-	ia = normalize_index(*temp_size_a, position.cost_a);
-	x = temp_b_values[ib];
-	remove_element_at_index(temp_b_values, temp_size_b, ib);
-	insert_element_at_index(temp_a_values, temp_size_a, ia, x);
-	return (rot + 1);
-}
-
-static int	calculate_candidate_score(t_position position __attribute__((unused)), int *temp_a_values,
-		int temp_size_a, t_selector_arena *arena, int rotation_cost)
-{
+	int	rotation_cost;
 	int	heuristic_estimate;
 
-	heuristic_estimate = get_estimatation(temp_a_values, temp_size_a,
+	rotation_cost = merged_cost(position.cost_a, position.cost_b) + 1;
+	heuristic_estimate = calculate_heuristic_without_copy(a_values, size_a,
 			arena->config.size_penalty_factor,
 			arena->config.heuristic_offset,
 			arena->config.heuristic_divisor);
 	return (heuristic_estimate + rotation_cost);
 }
 
-static bool	update_best_if_better(t_position *best_position, int *best_score,
-		t_position current_position, int current_score)
+static t_position	find_best_candidate(t_candidate *candidates, int count,
+		int *a_values, int size_a, t_selector_arena *arena)
 {
-	if (current_score < *best_score || (current_score == *best_score
-			&& is_better_position(current_position, *best_position)))
+	t_position	best_position;
+	int			i;
+	int			best_score;
+	int			current_score;
+
+	best_position = candidates[0].position;
+	best_score = calculate_score_without_simulation(best_position,
+			a_values, size_a, arena);
+	i = 1;
+	while (i < count)
 	{
-		*best_position = current_position;
-		*best_score = current_score;
-		return (true);
+		current_score = calculate_score_without_simulation(candidates[i].position,
+				a_values, size_a, arena);
+		if (current_score < best_score || (current_score == best_score
+				&& is_better_position(candidates[i].position, best_position)))
+		{
+			best_position = candidates[i].position;
+			best_score = current_score;
+		}
+		i++;
 	}
-	return (false);
+	return (best_position);
 }
 
 t_position	evaluate_with_lookahead(t_candidate *candidates,
 		t_selector_arena *arena)
 {
-	t_position	best_position;
-	int			i;
-	int			count;
-	int			best_score;
-	int			temp_size_a;
+	t_position	invalid_position;
 
-	best_position = validate_lookahead_input(candidates, arena);
-	if (best_position.total == INT_MAX)
-		return (best_position);
-	count = arena->top_k_count;
-	best_score = INT_MAX;
-	i = 0;
-	while (i < count)
+	if (!arena || !arena->snapshot_arena || !candidates || arena->top_k_count == 0)
 	{
-		copy_snapshot_to_temp(arena, &arena->temp_a_values, &arena->temp_b_values,
-			&temp_size_a, &arena->snapshot_arena->size_b);
-		update_best_if_better(&best_position, &best_score, candidates[i].position,
-			calculate_candidate_score(candidates[i].position, arena->temp_a_values,
-				temp_size_a, arena, simulate_move_operation(candidates[i].position,
-					arena->temp_a_values, arena->temp_b_values, &temp_size_a,
-					&arena->snapshot_arena->size_b)));
-		i++;
+		invalid_position.total = INT_MAX;
+		return (invalid_position);
 	}
-	return (best_position);
+	return (find_best_candidate(candidates, arena->top_k_count,
+			arena->snapshot_arena->a_values, arena->snapshot_arena->size_a, arena));
 }
