b8158e854ba032b2d456d59d895a447caf8bbcd3
tisonpatrik <patriktison@gmail.com>
2025-09-17 21:00:25 +0200

    clean up

--- push_swap/src/solver/align_min_to_top.c
+++ push_swap/src/solver/align_min_to_top.c
@@ -3,43 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   align_min_to_top.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:01:47 by ptison            #+#    #+#             */
-/*   Updated: 2025/09/16 21:08:34 by ptison           ###   ########.fr       */
+/*   Updated: 2025/09/17 20:56:33 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/selector.h"
 #include "../../include/solver.h"
 
-void	align_min_to_top(t_sorting_state *ps)
+static int	find_min_index(t_stack *stack)
 {
-	int		size_a;
-	int		min_idx;
-	int		min_val;
-	int		cost;
-	int		i;
-	t_node	*n;
+	int		min_index;
+	int		min_value;
+	int		current_index;
+	t_node	*current_node;
 
-	size_a = get_size(ps->a);
-	if (size_a == 0)
-		return ;
-	min_idx = 0;
-	min_val = get_content(get_head(ps->a));
-	n = get_head(ps->a);
-	i = 0;
-	while (n != NULL)
+	min_index = 0;
+	min_value = get_content(get_head(stack));
+	current_node = get_head(stack);
+	current_index = 0;
+	while (current_node != NULL)
 	{
-		if (get_content(n) < min_val)
+		if (get_content(current_node) < min_value)
 		{
-			min_val = get_content(n);
-			min_idx = i;
+			min_value = get_content(current_node);
+			min_index = current_index;
 		}
-		n = get_next(n);
-		i++;
+		current_node = get_next(current_node);
+		current_index++;
 	}
-	cost = signed_cost(min_idx, size_a);
+	return (min_index);
+}
+
+static void	perform_rotations(t_sorting_state *ps, int cost)
+{
 	while (cost > 0)
 	{
 		rotate_a(ps);
@@ -50,5 +49,19 @@ void	align_min_to_top(t_sorting_state *ps)
 		reverse_rotate_a(ps);
 		cost++;
 	}
+}
+
+void	align_min_to_top(t_sorting_state *ps)
+{
+	int		size_a;
+	int		min_idx;
+	int		cost;
+
+	size_a = get_size(ps->a);
+	if (size_a == 0)
+		return ;
+	min_idx = find_min_index(ps->a);
+	cost = signed_cost(min_idx, size_a);
+	perform_rotations(ps, cost);
 }
 
--- push_swap/src/solver/sort_three.c
+++ push_swap/src/solver/sort_three.c
@@ -6,53 +6,64 @@
 /*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 00:00:00 by patrik            #+#    #+#             */
-/*   Updated: 2025/09/16 21:40:58 by patrik           ###   ########.fr       */
+/*   Updated: 2025/09/17 20:59:35 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/ops.h"
 #include "../../include/stack.h"
 
-void	sort_three(t_sorting_state *ps)
+static void	get_three_values(t_stack *stack, int *first, int *second, int *third)
 {
-	t_node	*a;
-	t_node	*b;
-	t_node	*c;
-	int		x;
-	int		y;
-	int		z;
+	t_node	*first_node;
+	t_node	*second_node;
+	t_node	*third_node;
 
-	if (get_size(ps->a) != 3)
-		return ;
-	a = get_head(ps->a);
-	b = get_next(a);
-	c = get_next(b);
-	x = get_content(a);
-	y = get_content(b);
-	z = get_content(c);
-	if (x < y && y < z)
-		return ;
-	if (x > y && y < z && x < z)
+	first_node = get_head(stack);
+	second_node = get_next(first_node);
+	third_node = get_next(second_node);
+	*first = get_content(first_node);
+	*second = get_content(second_node);
+	*third = get_content(third_node);
+}
+
+static void	handle_sorting_cases(t_sorting_state *ps, int first, int second, int third)
+{
+	if (first > second && second < third && first < third)
 	{
 		swap_a(ps);
 		return ;
 	}
-	if (x > y && y > z)
+	if (first > second && second > third)
 	{
 		swap_a(ps);
 		reverse_rotate_a(ps);
 		return ;
 	}
-	if (x > y && y < z && x > z)
+	if (first > second && second < third && first > third)
 	{
 		rotate_a(ps);
 		return ;
 	}
-	if (x < y && y > z && x < z)
+	if (first < second && second > third && first < third)
 	{
 		swap_a(ps);
 		rotate_a(ps);
 		return ;
 	}
 	reverse_rotate_a(ps);
+}
+
+void	sort_three(t_sorting_state *ps)
+{
+	int		first_value;
+	int		second_value;
+	int		third_value;
+
+	if (get_size(ps->a) != 3)
+		return ;
+	get_three_values(ps->a, &first_value, &second_value, &third_value);
+	if (first_value < second_value && second_value < third_value)
+		return ;
+	handle_sorting_cases(ps, first_value, second_value, third_value);
 }
