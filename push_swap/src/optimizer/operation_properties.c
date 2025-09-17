3a8a1e322ae317a4b3f712c6b54e5a760912831d
tisonpatrik <patriktison@gmail.com>
2025-09-17 22:51:33 +0200

    optimalizer update

--- push_swap/src/optimizer/operation_properties.c
+++ push_swap/src/optimizer/operation_properties.c
@@ -6,7 +6,7 @@
 /*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:37:33 by patrik            #+#    #+#             */
-/*   Updated: 2025/09/16 21:40:58 by patrik           ###   ########.fr       */
+/*   Updated: 2025/09/17 22:51:05 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,24 @@
 
 bool	touches_a(t_operation op)
 {
-	switch (op)
-	{
-		case SA:
-		case RA:
-		case RRA:
-			return (true);
-		default:
-			return (false);
-	}
+	if (op == SA || op == RA || op == RRA)
+		return (true);
+	return (false);
 }
 
 
 bool	touches_b(t_operation op)
 {
-	switch (op)
-	{
-		case SB:
-		case RB:
-		case RRB:
-			return (true);
-		default:
-			return (false);
-	}
+	if (op == SB || op == RB || op == RRB)
+		return (true);
+	return (false);
 }
 
 bool	is_barrier(t_operation op)
 {
-	switch (op)
-	{
-		case SS:
-		case RR:
-		case RRR:
-		case PA:
-		case PB:
-			return (true);
-		default:
-			return (false);
-	}
+	if (op == SS || op == RR || op == RRR || op == PA || op == PB)
+		return (true);
+	return (false);
 }
 
 
--- push_swap/src/optimizer/optimizer.c
+++ push_swap/src/optimizer/optimizer.c
@@ -6,87 +6,79 @@
 /*   By: patrik <patrik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 21:37:30 by patrik            #+#    #+#             */
-/*   Updated: 2025/09/16 21:40:58 by patrik           ###   ########.fr       */
+/*   Updated: 2025/09/17 22:50:16 by patrik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/optimizer.h"
 
-void optimize_ops(t_list **seq)
+bool	validate_sequence(t_list **seq)
 {
 	if (!seq || !*seq)
-		return;
+		return (false);
 	
 	int len = ft_lstsize(*seq);
 	if (len < 2)
-		return;
+		return (false);
 	
-	const int max_bubble = 4;
+	return (true);
+}
+
+void	replace_sequence_if_changed(t_list **seq, t_list *new_seq, bool changed, bool *overall_changed)
+{
+	if (changed)
+	{
+		ft_lstclear(seq, free);
+		*seq = new_seq;
+		*overall_changed = true;
+	}
+	else
+	{
+		ft_lstclear(&new_seq, free);
+	}
+}
+
+void	apply_optimization_pass(t_list **seq, bool *changed)
+{
+	bool change;
+	t_list *new_seq;
+	
+	// Bubble operations
+	new_seq = bubble_across_other_stack(*seq, 4, &change);
+	replace_sequence_if_changed(seq, new_seq, change, changed);
+	
+	// Merge neighbors
+	new_seq = merge_neighbors(*seq, &change);
+	replace_sequence_if_changed(seq, new_seq, change, changed);
+	
+	// Cancel inverse pairs
+	new_seq = cancel_inverse_pairs(*seq, &change);
+	replace_sequence_if_changed(seq, new_seq, change, changed);
+	
+	// Cancel across other stack A
+	new_seq = cancel_across_other_stack_a(*seq, &change);
+	replace_sequence_if_changed(seq, new_seq, change, changed);
+	
+	// Cancel across other stack B
+	new_seq = cancel_across_other_stack_b(*seq, &change);
+	replace_sequence_if_changed(seq, new_seq, change, changed);
+}
+
+void	run_optimization_loop(t_list **seq)
+{
 	bool changed = true;
 	
 	while (changed)
 	{
 		changed = false;
-		bool c;
-		
-		t_list *new_seq = bubble_across_other_stack(*seq, max_bubble, &c);
-		if (c)
-		{
-			ft_lstclear(seq, free);
-			*seq = new_seq;
-			changed = true;
-		}
-		else
-		{
-			ft_lstclear(&new_seq, free);
-		}
-		
-		new_seq = merge_neighbors(*seq, &c);
-		if (c)
-		{
-			ft_lstclear(seq, free);
-			*seq = new_seq;
-			changed = true;
-		}
-		else
-		{
-			ft_lstclear(&new_seq, free);
-		}
-		
-		new_seq = cancel_inverse_pairs(*seq, &c);
-		if (c)
-		{
-			ft_lstclear(seq, free);
-			*seq = new_seq;
-			changed = true;
-		}
-		else
-		{
-			ft_lstclear(&new_seq, free);
-		}
-		
-		new_seq = cancel_across_other_stack_a(*seq, &c);
-		if (c)
-		{
-			ft_lstclear(seq, free);
-			*seq = new_seq;
-			changed = true;
-		}
-		else
-		{
-			ft_lstclear(&new_seq, free);
-		}
-		
-		new_seq = cancel_across_other_stack_b(*seq, &c);
-		if (c)
-		{
-			ft_lstclear(seq, free);
-			*seq = new_seq;
-			changed = true;
-		}
-		else
-		{
-			ft_lstclear(&new_seq, free);
-		}
+		apply_optimization_pass(seq, &changed);
 	}
+}
+
+void optimize_ops(t_list **seq)
+{
+	if (!validate_sequence(seq))
+		return;
+	
+	run_optimization_loop(seq);
 }
