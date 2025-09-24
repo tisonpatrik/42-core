08328d1519b2d72a0fb3e521153e78e04fe5c7fd
tisonpatrik <patriktison@gmail.com>
2025-09-24 17:15:16 +0200

    update

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: ptison <ptison@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 21:14:51 by ptison            #+#    #+#             */
-/*   Updated: 2025/09/24 15:56:21 by ptison           ###   ########.fr       */
+/*   Updated: 2025/09/24 17:11:27 by ptison           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,20 +51,19 @@ static void	send_message(pid_t pid, char *message)
 	}
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	pid_t				pid;
-	struct sigaction	sa;
+    if (argc < 3) exit(EXIT_FAILURE);
 
-	if (argc < 3)
-		exit(EXIT_FAILURE);
-	pid = ft_atoi(argv[1]);
-	if (pid < 1)
-		exit(EXIT_FAILURE);
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = signal_handler;
-	sigaction(SIGUSR2, &sa, NULL);
-	send_message(pid, argv[2]);
-	return (EXIT_SUCCESS);
+    pid_t pid = (pid_t)ft_atoi(argv[1]);
+    if (pid < 1) exit(EXIT_FAILURE);
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = signal_handler;
+    sigaction(SIGUSR2, &sa, NULL);
+
+    send_message(pid, argv[2]);
+    return EXIT_SUCCESS;
 }
