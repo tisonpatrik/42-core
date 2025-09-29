698ea9e6bed88debe5d3607dc15882412c4862b0
tisonpatrik <patriktison@gmail.com>
2025-09-29 19:13:47 +0200

    refactoting

--- push_swap/src/validator/input_parser.c
+++ push_swap/src/validator/input_parser.c
@@ -6,26 +6,38 @@
 /*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 21:00:04 by ptison            #+#    #+#             */
-/*   Updated: 2025/09/29 19:04:37 by ptison           ###   ########.fr       */
+/*   Updated: 2025/09/29 19:11:48 by ptison           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/validator.h"
 
+/*
+	Assume duplicates if malloc fails
+*/
 static bool	has_duplicates(int *arr, int n)
 {
+	int	*copy;
 	int	i;
 
 	if (n < 2)
 		return (false);
-	ft_heap_sort(arr, n);
+	copy = malloc(sizeof(int) * n);
+	if (!copy)
+		return (true);
+	ft_memcpy(copy, arr, sizeof(int) * n);
+	ft_heap_sort(copy, n);
 	i = 1;
 	while (i < n)
 	{
-		if (arr[i] == arr[i - 1])
+		if (copy[i] == copy[i - 1])
+		{
+			free(copy);
 			return (true);
+		}
 		i++;
 	}
+	free(copy);
 	return (false);
 }
 
@@ -55,7 +67,8 @@ t_parser_result	parse_args(int argc, char *argv[])
 	if (!buf)
 		return (create_parser_result(NULL, 0, FAILURE));
 	fill_numbers(argc, argv, buf);
-	if (has_duplicates(buf, count.count)||!has_negative_numbers(buf, count.count))
+	if (has_duplicates(buf, count.count) || !has_negative_numbers(buf,
+			count.count))
 		return (create_parser_result(buf, count.count, FAILURE));
 	return (create_parser_result(buf, count.count, SUCCESS));
 }
