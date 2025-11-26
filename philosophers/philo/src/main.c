e81d42140dad423bf541a87fc0066147f5d3a1f8
tisonpatrik <patriktison@gmail.com>
2025-11-26 16:42:22 +0100

    validator

--- philosophers/philo/src/main.c
+++ philosophers/philo/src/main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ptison <ptison@student.42prague.com>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/04 20:28:24 by ptison            #+#    #+#             */
-/*   Updated: 2025/11/04 20:34:25 by ptison           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../include/validator.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +8,7 @@ int main(int ac, char *av[]) {
     printf("Error, too few arguments\n");
     exit(EXIT_FAILURE);
   } else if ((ac == 5) || (ac == 6)) {
-    inputs = get_inputs(ac - 1, av);
+    inputs = get_inputs(ac, av);
   } else {
     printf("Error, too much arguments\n");
     exit(EXIT_FAILURE);
--- philosophers/philo/src/validator.c
+++ philosophers/philo/src/validator.c
@@ -1,8 +1,34 @@
 
 
 #include "../include/validator.h"
+#include <stdbool.h>
+#include <stdlib.h>
 
 t_inputs get_input(int count, char *arguments[]) {
-    t_inputs inputs;
-
+  char *endptr;
+  endptr = NULL;
+  errno = 0;
+  int count_of_philosophers = ft_strtoi10(arguments[1], &endptr);
+  int time_to_die = ft_strtoi10(arguments[2], &endptr);
+  int time_to_eat = ft_strtoi10(arguments[3], &endptr);
+  int time_to_sleep = ft_strtoi10(arguments[4], &endptr);
+  if (*endptr != '\0' || errno == ERANGE) {
+    exit(0);
+  }
+  t_inputs inputs;
+  inputs.count_of_philosophers = count_of_philosophers;
+  inputs.time_to_die = time_to_die;
+  inputs.time_to_eat = time_to_eat;
+  inputs.time_to_sleep = time_to_sleep;
+  if (count == 6)
+  {
+      int number_of_times_each_philosopher_must_eat = ft_strtoi10(arguments[5], &endptr);
+      if (*endptr != '\0' || errno == ERANGE) {
+        exit(0);
+      }
+      inputs.number_of_times_each_philosopher_must_eat=number_of_times_each_philosopher_must_eat;
+      inputs.is_restricted= true;
+  }
+  inputs.is_restricted = false;
+  return inputs;
 }
