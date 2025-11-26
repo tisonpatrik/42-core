3abd1082d3c1e1c84966ac6f7bae7b9e074b7202
tisonpatrik <patriktison@gmail.com>
2025-11-26 16:11:25 +0100

    philos and libft

--- philosophers/philo/src/main.c
+++ philosophers/philo/src/main.c
@@ -10,26 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/validator.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int	main(int ac, char **av)
-{
-	if (ac < 5)
-	{
-		printf("Error, too few arguments\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (ac == 5)
-	{
-	}
-	else if (ac == 6)
-	{
-	}
-	else
-	{
-		printf("Error, too much arguments\n");
-		exit(EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
+int main(int ac, char *av[]) {
+  t_inputs inputs;
+  if (ac < 5) {
+    printf("Error, too few arguments\n");
+    exit(EXIT_FAILURE);
+  } else if ((ac == 5) || (ac == 6)) {
+    inputs = get_inputs(ac - 1, av);
+  } else {
+    printf("Error, too much arguments\n");
+    exit(EXIT_FAILURE);
+  }
+  return (EXIT_SUCCESS);
 }
---
+++ philosophers/philo/src/validator.c
@@ -0,0 +1,8 @@
+
+
+#include "../include/validator.h"
+
+t_inputs get_input(int count, char *arguments[]) {
+    t_inputs inputs;
+
+}
