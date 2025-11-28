c2a6525adb4aa8ce6c7a65ed6bff5b90be51b917
tisonpatrik <patriktison@gmail.com>
2025-11-28 17:15:28 +0100

    arena setup

--- philosophers/philo/include/simulator.h
+++ philosophers/philo/include/simulator.h
@@ -3,26 +3,44 @@
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
+#include <bits/pthreadtypes.h>
 #include <stdbool.h>
 #include "../include/validator.h"
 
-typedef struct s_philosopher{
-    int id; bool is_eating;
-    bool is_sleeping;
-    bool is_thinking; } t_philosopher;
+typedef enum e_philosopher_state{
+    IS_THINKING,
+    IS_SLEEPING,
+    IS_EATING
+}   t_philosopher_state;
+
 
 
 typedef struct s_fork{
     int id;
     bool is_used; } t_fork;
 
-typedef struct s_simulation{
-    int count_of_philosophers;
-    int count_of_forks;
+
+typedef struct s_philosopher{
+    int id;
+    pthread_t thread;
+    int times_eaten;
+    long long last_meal_time;
+    long long state_start_time;
+    t_philosopher_state state;
+    int left_fork_id;
+    int right_fork_id;
     int time_to_die;
     int time_to_eat;
     int time_to_sleep;
     int number_of_times_each_philosopher_must_eat;
+    long long start_time;
+    pthread_mutex_t print_mutex;
+    pthread_mutex_t death_mutex;
+} t_philosopher;
+
+typedef struct s_simulation{
+    int count_of_philosophers;
+    int count_of_forks;
     t_philosopher* philosophers;
     t_fork* forks;
 }        t_simulation;
